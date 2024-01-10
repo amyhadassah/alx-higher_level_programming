a0b3f8d8a132d37fa455403c56f35c7b8c956afa
Amy Hadassah <amyhadassahbb@gmail.com>
2024-01-10 10:29:07 -0800

    Task 103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,30 +1,63 @@
 #include <Python.h>
 #include <object.h>
+#include <listobject.h>
 #include <bytesobject.h>
 
-void print_python_bytes(PyObject *p) {
+void print_python_list(PyObject *p)
+{
+	long size;
+	PyListObject *list;
+	PyObject *item;
+	const char *type_name;
+	int i;
+
+	printf("[*] Python list info\n");
+
+	if (!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+
+	list = (PyListObject *)p;
+	size = ((PyVarObject *)p)->ob_size;
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = list->ob_item[i];
+		type_name = (item->ob_type)->tp_name;
+		printf("Element %d: %s\n", i, type_name);
+	}
+}
+
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bytes;
 	char *string;
-	Py_ssize_t size, i;
+	long size;
+	int i;
 
 	printf("[.] bytes object info\n");
 
-	if (!PyBytes_Check(p)) {
+	if (!PyBytes_Check(p))
+	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 
+	bytes = (PyBytesObject *)p;
 	size = ((PyVarObject *)p)->ob_size;
-	string = ((PyBytesObject *)p)->ob_sval;
+	string = bytes->ob_sval;
 
-	printf("  size: %zd\n", size);
+	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", string);
+	printf("  first %ld bytes:", (size < 10) ? size : 10);
 
-	printf("  first %zd bytes:", size < 10 ? size + 1 : 10);
-	for (i = 0; i < size && i < 10; i++) {
+	for (i = 0; i < size && i < 10; i++)
+	{
 		printf(" %02x", (unsigned char)string[i]);
-	}
-	if (size > 10) {
-		printf(" ...");
 	}
 	printf("\n");
 }
